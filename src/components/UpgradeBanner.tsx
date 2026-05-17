25519cf2c71d4fe5be87426b50df43a252d88d66
Saurabh2619 <sharmasaurabh2606@gmail.com>
2026-05-17 15:26:00 +0530

    hide react

--- src/components/UpgradeBanner.tsx
+++ src/components/UpgradeBanner.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { motion } from 'framer-motion';
 import { Crown, ArrowRight } from 'lucide-react';
 import { usePaywall } from '../hooks/usePaywall';
